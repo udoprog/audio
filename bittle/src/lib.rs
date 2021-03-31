7f4dd7957defb7ee582518ae460d510b76e0296b
John-John Tedro <udoprog@tedro.se>
2021-03-31 15:12:35 +0200

    Fixed doc comment, release bittle 0.1.1

--- bittle/src/lib.rs
+++ bittle/src/lib.rs
@@ -4,24 +4,24 @@
 //! [testing][Mask::test]. This allows for masking indexes in certain
 //! operations. Like if you want to mask which channels in an audio buffer is in
 //! use or not.
-///
-/// # Examples
-///
-/// ```rust
-/// fn test<M>(mask: M) where M: bittle::Mask {
-///     assert!(!mask.test(0));
-///     assert!(mask.test(1));
-/// }
-///
-/// let mut set = bittle::BitSet::<u16>::empty();
-/// set.set(1);
-///
-/// test(&set);
-///
-/// set.clear(1);
-///
-/// assert_eq!(std::mem::size_of_val(&set), std::mem::size_of::<u16>());
-/// ```
+//!
+//! # Examples
+//!
+//! ```rust
+//! fn test<M>(mask: M) where M: bittle::Mask {
+//!     assert!(!mask.test(0));
+//!     assert!(mask.test(1));
+//! }
+//!
+//! let mut set = bittle::BitSet::<u16>::empty();
+//! set.set(1);
+//!
+//! test(&set);
+//!
+//! set.clear(1);
+//!
+//! assert_eq!(std::mem::size_of_val(&set), std::mem::size_of::<u16>());
+//! ```
 
 #[macro_use]
 mod macros;
