8395870faa9bebf33d25e1a06d10e9a2e88ad1f4
John-John Tedro <udoprog@tedro.se>
2021-04-07 21:43:52 +0200

    Add two benches

--- ste/benches/benches.rs
+++ ste/benches/benches.rs
@@ -18,3 +18,51 @@ fn test_on_ste(b: &mut Bencher) {
         result
     });
 }
+
+#[bench]
+fn count_to_1000_ste(b: &mut Bencher) {
+    b.iter(|| {
+        let thread = ste::Thread::new().unwrap();
+        let mut total = 0u32;
+
+        for n in 0..1000u32 {
+            total += thread.submit(move || n + 1).unwrap();
+        }
+
+        thread.join().unwrap();
+        assert_eq!(total, 500500);
+        total
+    });
+}
+
+#[bench]
+fn count_to_1000_mpsc(b: &mut Bencher) {
+    use std::sync::mpsc;
+    use std::thread;
+
+    b.iter(|| {
+        let mut total = 0u32;
+
+        let t = {
+            let (tx, rx) = mpsc::sync_channel(0);
+            let (out_tx, out_rx) = mpsc::sync_channel(0);
+
+            let t = thread::spawn(move || {
+                while let Ok(task) = rx.recv() {
+                    out_tx.send(task + 1).unwrap();
+                }
+            });
+
+            for n in 0..1000u32 {
+                tx.send(n).unwrap();
+                total += out_rx.recv().unwrap();
+            }
+
+            t
+        };
+
+        t.join().unwrap();
+        assert_eq!(total, 500500);
+        total
+    });
+}
