7a6bae31b68b22e2920de244fafa48bc923b325d
John-John Tedro <udoprog@tedro.se>
2021-04-12 06:53:39 +0200

    Go back to modifiers lock

--- ste/src/worker.rs
+++ ste/src/worker.rs
@@ -2,6 +2,7 @@ use crate::adapter::Adapter;
 use crate::linked_list::{LinkedList, Node};
 use crate::loom::sync::atomic::{AtomicIsize, Ordering};
 use crate::loom::sync::Mutex;
+use crate::loom::thread;
 use crate::parker::Parker;
 use crate::tagged::Tag;
 use crate::Panicked;
@@ -12,14 +13,10 @@ use std::task::Waker;
 /// The type of the prelude function.
 pub(super) type Prelude = dyn Fn() + Send + 'static;
 
-struct Locked {
-    running: bool,
-    queue: LinkedList<Entry>,
-}
-
 // Shared state between the worker thread and [Thread].
 pub(super) struct Shared {
-    locked: Mutex<Locked>,
+    modifiers: AtomicIsize,
+    queue: Mutex<LinkedList<Entry>>,
     parker: Parker,
 }
 
@@ -27,25 +24,60 @@ impl Shared {
     /// Construct new shared state.
     pub(super) fn new() -> Self {
         Self {
-            locked: Mutex::new(Locked {
-                running: true,
-                queue: LinkedList::new(),
-            }),
+            modifiers: AtomicIsize::new(0),
+            queue: Mutex::new(LinkedList::new()),
             parker: Parker::new(),
         }
+    }
+
+    /// Construct a guard which while held ensures that the system knows someone
+    /// is modifying the worker queue.
+    ///
+    /// Interior cleanup of the worker queue will only be considered complete
+    /// once modifiers reaches 0, because otherwise we run the risk of another
+    /// thread being in the middle of a modification while we are cleaning up
+    /// and we leave that thread in a blocked state.
+    pub(super) fn lock_queue(&self) -> Option<ModifierGuard<'_>> {
+        let value = self.modifiers.fetch_add(1, Ordering::SeqCst);
+
+        if value == isize::MAX {
+            // We don't have much choice here. Wrapping around is very unlikely
+            // to happen because the number of threads required for it to happen
+            // is so big. We eprintln in an attempt to get some information out
+            // but we really need to abort to maintain the safety of the system.
+            eprintln!("ste: modifiers invariant breached, aborting");
+            std::process::abort();
+        }
+
+        if value < 0 {
+            self.modifiers.fetch_sub(1, Ordering::SeqCst);
+            return None;
+        }
+
+        Some(ModifierGuard {
+            modifiers: &self.modifiers,
+        })
     }
 
     // Release all shared state, this will hang until the number of modifiers is
     // zero, after which it will pop all elements from the queue and release
     // them.
     unsafe fn panic_join(&self) {
-        let mut local = {
-            let mut guard = self.locked.lock().unwrap();
-            debug_assert!(guard.running);
-            guard.running = false;
-            guard.queue.steal()
-        };
+        let modifiers = self.modifiers.fetch_add(isize::MIN, Ordering::SeqCst);
 
+        // It's not possible for the state to be anything but empty
+        // here, because the worker thread takes the state before
+        // executing user code which might panic.
+        debug_assert!(modifiers >= 0);
+
+        let mut local = self.queue.lock().unwrap().steal();
+        release_local_queue(&mut local);
+
+        while self.modifiers.load(Ordering::Acquire) != isize::MIN {
+            thread::yield_now();
+        }
+
+        let mut local = self.queue.lock().unwrap().steal();
         release_local_queue(&mut local);
     }
 
@@ -64,13 +96,15 @@ impl Shared {
         let mut node = Node::new(entry);
 
         let first = {
-            let mut guard = self.locked.lock().unwrap();
+            let _guard = match self.lock_queue() {
+                Some(guard) => guard,
+                None => return Err(Panicked(())),
+            };
 
-            if !guard.running {
-                return Err(Panicked(()));
-            }
-
-            guard.queue.push_front(ptr::NonNull::from(&mut node))
+            self.queue
+                .lock()
+                .unwrap()
+                .push_front(ptr::NonNull::from(&mut node))
         };
 
         if first {
@@ -93,7 +127,11 @@ impl Shared {
     /// We mark the modifiers count as negative to signal any entering threads
     /// that they are no longer permitted to push tasks onto the task set.
     pub(super) fn outer_join(&self) {
-        self.locked.lock().unwrap().running = false;
+        // We get the thread to shut down by disallowing the queue to be
+        // modified. If the thread has already shut down (due to a panic)
+        // this will already have been set to `isize::MIN` and will wrap
+        // around or do some other nonsense we can ignore.
+        self.modifiers.fetch_add(isize::MIN, Ordering::SeqCst);
         self.parker.unpark();
     }
 }
@@ -120,17 +158,9 @@ pub(super) fn run(prelude: Option<Box<Prelude>>, shared: ptr::NonNull<Shared>) {
             mem::forget(guard);
         }
 
-        loop {
-            let mut local = {
-                let mut guard = shared.locked.lock().unwrap();
-
-                if !guard.running {
-                    release_local_queue(&mut guard.queue);
-                    break;
-                }
-
-                guard.queue.steal()
-            };
+        while let Some(guard) = shared.lock_queue() {
+            let mut local = shared.queue.lock().unwrap().steal();
+            drop(guard);
 
             if local.is_empty() {
                 shared.parker.park();
