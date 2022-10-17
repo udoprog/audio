5d679093d1821843f8f499ceb034db42e6b9d5a9
John-John Tedro <udoprog@tedro.se>
2022-10-17 13:00:19 +0200

    Update Documentation and Cargo.toml

--- ste/README.md
+++ ste/README.md
@@ -1,8 +1,9 @@
 # ste
 
-[![Documentation](https://docs.rs/ste/badge.svg)](https://docs.rs/ste)
-[![Crates](https://img.shields.io/crates/v/ste.svg)](https://crates.io/crates/ste)
-[![Actions Status](https://github.com/udoprog/audio/workflows/Rust/badge.svg)](https://github.com/udoprog/audio/actions)
+[<img alt="github" src="https://img.shields.io/badge/github-udoprog/audio-8da0cb?style=for-the-badge&logo=github" height="20">](https://github.com/udoprog/audio)
+[<img alt="crates.io" src="https://img.shields.io/crates/v/ste.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/ste)
+[<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-ste-66c2a5?style=for-the-badge&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K" height="20">](https://docs.rs/ste)
+[<img alt="build status" src="https://img.shields.io/github/workflow/status/udoprog/audio/CI/main?style=for-the-badge" height="20">](https://github.com/udoprog/audio/actions?query=branch%3Amain)
 
 A single-threaded executor with some tricks up its sleeve.
 
@@ -28,6 +29,8 @@ assert_eq!(20, n);
 
 thread.join();
 ```
+
+<br>
 
 ## Restricting thread access using tags
 
@@ -86,11 +89,15 @@ foo.say_hello(); // <- Oops, panics!
 thread.join();
 ```
 
+<br>
+
 ## Known unsafety and soundness issues
 
 Below you can find a list of unsafe use and known soundness issues this
 library currently has. The soundness issues **must be fixed** before this
 library goes out of *alpha*.
+
+<br>
 
 ### Pointers to stack-local addresses
 
@@ -104,6 +111,8 @@ library), it carries a risk that if the background thread were to proceed
 executing a task that is no longer synchronized properly with a caller of
 [submit] it might end up referencing data which is either no longer valid
 (use after free), or contains something else (dirty).
+
+<br>
 
 ### Tag re-use
 
@@ -121,5 +130,3 @@ guarantee uniqueness.
 [submit]: https://docs.rs/ste/*/ste/struct.Thread.html#method.submit
 [Tag]: https://docs.rs/ste/*/ste/struct.Tag.html
 [Thread]: https://docs.rs/ste/*/ste/struct.Thread.html
-
-License: MIT/Apache-2.0
--- ste/src/lib.rs
+++ ste/src/lib.rs
@@ -1,6 +1,7 @@
-//! [![Documentation](https://docs.rs/ste/badge.svg)](https://docs.rs/ste)
-//! [![Crates](https://img.shields.io/crates/v/ste.svg)](https://crates.io/crates/ste)
-//! [![Actions Status](https://github.com/udoprog/audio/workflows/Rust/badge.svg)](https://github.com/udoprog/audio/actions)
+//! [<img alt="github" src="https://img.shields.io/badge/github-udoprog/audio-8da0cb?style=for-the-badge&logo=github" height="20">](https://github.com/udoprog/audio)
+//! [<img alt="crates.io" src="https://img.shields.io/crates/v/ste.svg?style=for-the-badge&color=fc8d62&logo=rust" height="20">](https://crates.io/crates/ste)
+//! [<img alt="docs.rs" src="https://img.shields.io/badge/docs.rs-ste-66c2a5?style=for-the-badge&logoColor=white&logo=data:image/svg+xml;base64,PHN2ZyByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDUxMiA1MTIiPjxwYXRoIGZpbGw9IiNmNWY1ZjUiIGQ9Ik00ODguNiAyNTAuMkwzOTIgMjE0VjEwNS41YzAtMTUtOS4zLTI4LjQtMjMuNC0zMy43bC0xMDAtMzcuNWMtOC4xLTMuMS0xNy4xLTMuMS0yNS4zIDBsLTEwMCAzNy41Yy0xNC4xIDUuMy0yMy40IDE4LjctMjMuNCAzMy43VjIxNGwtOTYuNiAzNi4yQzkuMyAyNTUuNSAwIDI2OC45IDAgMjgzLjlWMzk0YzAgMTMuNiA3LjcgMjYuMSAxOS45IDMyLjJsMTAwIDUwYzEwLjEgNS4xIDIyLjEgNS4xIDMyLjIgMGwxMDMuOS01MiAxMDMuOSA1MmMxMC4xIDUuMSAyMi4xIDUuMSAzMi4yIDBsMTAwLTUwYzEyLjItNi4xIDE5LjktMTguNiAxOS45LTMyLjJWMjgzLjljMC0xNS05LjMtMjguNC0yMy40LTMzLjd6TTM1OCAyMTQuOGwtODUgMzEuOXYtNjguMmw4NS0zN3Y3My4zek0xNTQgMTA0LjFsMTAyLTM4LjIgMTAyIDM4LjJ2LjZsLTEwMiA0MS40LTEwMi00MS40di0uNnptODQgMjkxLjFsLTg1IDQyLjV2LTc5LjFsODUtMzguOHY3NS40em0wLTExMmwtMTAyIDQxLjQtMTAyLTQxLjR2LS42bDEwMi0zOC4yIDEwMiAzOC4ydi42em0yNDAgMTEybC04NSA0Mi41di03OS4xbDg1LTM4Ljh2NzUuNHptMC0xMTJsLTEwMiA0MS40LTEwMi00MS40di0uNmwxMDItMzguMiAxMDIgMzguMnYuNnoiPjwvcGF0aD48L3N2Zz4K" height="20">](https://docs.rs/ste)
+//! [<img alt="build status" src="https://img.shields.io/github/workflow/status/udoprog/audio/CI/main?style=for-the-badge" height="20">](https://github.com/udoprog/audio/actions?query=branch%3Amain)
 //!
 //! A single-threaded executor with some tricks up its sleeve.
 //!
@@ -26,10 +27,12 @@
 //! assert_eq!(20, n);
 //!
 //! thread.join();
-//! # Ok(()) }    
+//! # Ok(()) }
 //! ```
 //!
-//! # Restricting thread access using tags
+//! <br>
+//!
+//! ## Restricting thread access using tags
 //!
 //! This library provides the ability to construct a [Tag] which is uniquely
 //! associated with the thread that created it. This can then be used to ensure
@@ -95,13 +98,17 @@
 //! # Ok(()) }
 //! ```
 //!
-//! # Known unsafety and soundness issues
+//! <br>
+//!
+//! ## Known unsafety and soundness issues
 //!
 //! Below you can find a list of unsafe use and known soundness issues this
 //! library currently has. The soundness issues **must be fixed** before this
 //! library goes out of *alpha*.
 //!
-//! ## Pointers to stack-local addresses
+//! <br>
+//!
+//! ### Pointers to stack-local addresses
 //!
 //! In order to efficiently share data between a thread calling [submit] and the
 //! background thread, the background thread references a fair bit of
@@ -114,7 +121,9 @@
 //! [submit] it might end up referencing data which is either no longer valid
 //! (use after free), or contains something else (dirty).
 //!
-//! ## Tag re-use
+//! <br>
+//!
+//! ### Tag re-use
 //!
 //! [Tag] containers currently use a tag based on the address of a slab of
 //! allocated memory that is associated with each [Thread]. If however a
@@ -272,7 +281,7 @@ impl Thread {
     /// assert_eq!(20, n);
     ///
     /// thread.join();
-    /// # Ok(()) }    
+    /// # Ok(()) }
     /// ```
     ///
     /// Unwinding panics as isolated on a per-task basis.
@@ -463,7 +472,7 @@ impl Thread {
     /// assert_eq!(20, n);
     ///
     /// thread.join();
-    /// # Ok(()) }    
+    /// # Ok(()) }
     /// ```
     pub fn join(mut self) {
         if let Some(handle) = self.handle.take() {
